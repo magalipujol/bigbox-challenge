d09c8b4b470f18b6ffa5f5c42cc98b40faf6bb51
magalipujol <magalicpujol@gmail.com>
2021-10-24 21:26:22 -0300

    un poco de estilo a las actividades similares

---
+++ src/components/PhotosCarousel.vue
@@ -0,0 +1,18 @@
+<template>
+  <Carousel>
+    <Slide> Slide 1 Content </Slide>
+    <Slide> Slide 2 Content </Slide>
+  </Carousel>
+</template>
+
+<script>
+// import { Carousel, Slide } from 'vue-carousel'
+const { Carousel, Slide } = require('vue-carousel')
+export default {
+  name: 'Carousel',
+  components: {
+    Carousel,
+    Slide
+  }
+}
+</script>
--- src/components/SimilarActivities.vue
+++ src/components/SimilarActivities.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="d-flex flex-wrap">
-    <p>Actividades similares:</p>
-    <ActivityCard
-      v-for="(activity, index) in activities"
-      :key="index"
-      :activity="activity"
-    ></ActivityCard>
+  <div class="container">
+    <p class="fs-1 activity-text activity-subtitle">Actividades similares:</p>
+    <div class="d-flex flex-wrap">
+      <ActivityCard
+        v-for="(activity, index) in activities"
+        :key="index"
+        :activity="activity"
+      ></ActivityCard>
+    </div>
   </div>
 </template>
 
@@ -21,3 +23,22 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.activity-text {
+  font-family: Quicksand;
+  font-style: normal;
+  color: #464646;
+  text-align: left;
+  font-weight: 400;
+  size: 16px;
+}
+.activity-subtitle {
+  font-size: 22px;
+  font-weight: 500;
+  line-height: 28px;
+  letter-spacing: -1px;
+  margin-top: 40px;
+  letter-spacing: -1;
+}
+</style>
